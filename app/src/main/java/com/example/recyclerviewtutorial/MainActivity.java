f88adf22b7b1a6d0a80a7636821de58b22a4162c
mamba007 <ga1998urav@gmail.com>
2021-01-01 12:38:31 +0530

    Added few more Views to recyclerview

--- app/src/main/java/com/example/recyclerviewtutorial/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtutorial/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Display;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
         list.add(new Model(R.drawable.ic_image1,"Disables","Handicapped"));
         list.add(new Model(R.drawable.ic_baseline_account_box_24,"User","User PRofile"));
         list.add(new Model(R.drawable.ic_baseline_emoji_people_24,"Emoji","This is a Emoji"));
+        list.add(new Model(R.drawable.ic_baseline_people_24,"People","This is a Prople"));
+        list.add(new Model(R.drawable.ic_baseline_people_24,"People","This is a Prople"));
+        list.add(new Model(R.drawable.ic_baseline_people_24,"People","This is a Prople"));
+
 
         namesAdapter = new NamesAdapter(list,this);
         recyclerView.setAdapter(namesAdapter);
